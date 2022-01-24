a3db5e6d345fd2c40827373ae7d3b34780adef69
kyle <kyle41111@gmail.com>
2022-01-24 16:25:05 -0500

    Update autocomplete.ps1

--- ActiveDirectory/autocomplete.ps1
+++ ActiveDirectory/autocomplete.ps1
@@ -1,6 +1,8 @@
+$member = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+Add-LocalGroupMember -Group "Administrators" -Member "$member"
 Set-ExecutionPolicy unrestricted
 New-Item -Path $Profile -Type File -Force
-Add-Content -Path C:\Users\Administrator\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Value 'Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Add-Content -Path C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 -Value 'Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
